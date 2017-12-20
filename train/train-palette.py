031abb52830260ad357468d7f0439ea50d20d57a
Peter Orum <peter@peterorum.com>
2017-12-20 11:15:08 +1100

    plot

--- train/train-palette.py
+++ train/train-palette.py
@@ -162,20 +162,13 @@ print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 
 # list all data in history
-print(history.history.keys())
+# print(history.history.keys())
+
 # summarize history for accuracy
 plt.plot(history.history['acc'])
 plt.plot(history.history['val_acc'])
 plt.title('model accuracy')
 plt.ylabel('accuracy')
 plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
-# summarize history for loss
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
+plt.legend(['train', 'val'], loc='upper left')
 plt.show()
