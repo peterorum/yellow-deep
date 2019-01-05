506eef8f5c3cdc4759a85643ad88ba46009c4ed9
Peter Orum <peter@peterorum.com>
2019-01-05 18:29:09 +1100

    add target

--- generate/convert-to-csv.py
+++ generate/convert-to-csv.py
@@ -21,7 +21,7 @@ max_bins = 10
 bins = max_bins - 1  # rounding
 
 # load data
-palettes = [p['colors'] for p in data]
+palettes = [{'colors': p['colors'], 'selected': p['selected'] if 'selected' in p else False} for p in data]
 
 # convert each full color into its binned string version
 # eg hsl 0.5, 0.5, 0.5 -> hsl-050-050-050, depending on the number of bins
@@ -31,13 +31,13 @@ palettes = [p['colors'] for p in data]
 color_bins = []
 
 for palette in palettes:
-    hsl = [[c['h'], c['s'], c['l']] for c in palette]
+    hsl = [[c['h'], c['s'], c['l']] for c in palette['colors']]
 
     hsl = [[int(np.round(np.round(x * bins) / bins * 100)) for x in color] for color in hsl]
 
     hsl = [f'hsl-{x[0]:03d}-{x[1]:03d}-{x[2]:03d}' for x in hsl]
 
-    color_bins.append(hsl)
+    color_bins.append({'hsl': hsl, 'selected': palette['selected']})
 
 # now count occurences of each possible combination
 
@@ -68,19 +68,21 @@ for cb in color_bins:
 
     # count them
 
-    for c in cb:
+    for c in cb['hsl']:
         dic[c] += 1
 
     # store counted version
-    encoded_colors.append(dic)
+    encoded_colors.append({'colors': dic, 'selected': cb['selected']})
 
 # dump counted version to csv, with all colours as header
 
 csv_filename = '../data/manually-selected-palettes.csv'
 
 with open(csv_filename, 'w') as outfile:
+    outfile.write('selected,')
     outfile.writelines([','.join(all_colors), '\n'])
 
     for e in encoded_colors:
-        outfile.write(','.join(str(e[c]) for c in all_colors))
+        outfile.write('1,' if e['selected'] else '0,')
+        outfile.write(','.join(str(e['colors'][c]) for c in all_colors))
         outfile.write('\n')
