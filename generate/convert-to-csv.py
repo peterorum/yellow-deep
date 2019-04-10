4167dcc15ad235d509d0e5f0c764d563ef46018f
Peter Orum <peter@peterorum.com>
2019-04-10 14:16:06 +1000

    overfitted

--- generate/convert-to-csv.py
+++ generate/convert-to-csv.py
@@ -39,7 +39,24 @@ def convert_to_csv(file):
 
         hsl = [f'hsl-{x[0]:03d}-{x[1]:03d}-{x[2]:03d}' for x in hsl]
 
-        color_bins.append({'id': palette['id'], 'hsl': hsl, 'selected': palette['selected']})
+        h_std = np.std([c['h'] for c in palette['colors']])
+        s_std = np.std([c['s'] for c in palette['colors']])
+        l_std = np.std([c['l'] for c in palette['colors']])
+
+        s_avg = np.average([c['s'] for c in palette['colors']])
+        l_avg = np.average([c['l'] for c in palette['colors']])
+
+        s_min = np.min([c['s'] for c in palette['colors']])
+        l_min = np.min([c['l'] for c in palette['colors']])
+
+        s_max = np.max([c['s'] for c in palette['colors']])
+        l_max = np.max([c['l'] for c in palette['colors']])
+
+        color_bins.append({'id': palette['id'], 'hsl': hsl, 'selected': palette['selected'],
+                           'h_std': h_std, 's_std': s_std, 'l_std': l_std,
+                           's_avg': s_avg, 'l_avg': l_avg,
+                           's_min': s_min, 'l_min': l_min,
+                           's_max': s_max, 'l_max': l_max})
 
     # now count occurences of each possible combination
 
@@ -74,18 +91,26 @@ def convert_to_csv(file):
             dic[c] += 1
 
         # store counted version
-        encoded_colors.append({'id': cb['id'], 'colors': dic, 'selected': cb['selected']})
+        encoded_colors.append({'id': cb['id'], 'colors': dic, 'selected': cb['selected'],
+                               'h_std': cb['h_std'], 's_std': cb['s_std'], 'l_std': cb['l_std'],
+                               's_avg': cb['s_avg'], 'l_avg': cb['l_avg'],
+                               's_min': cb['s_min'], 'l_min': cb['l_min'],
+                               's_max': cb['s_max'], 'l_max': cb['l_max']})
 
     # dump counted version to csv, with all colours as header
 
     with open(csv_file, 'w') as outfile:
         outfile.write('id,selected,')
-        outfile.writelines([','.join(all_colors), '\n'])
+        outfile.writelines([','.join(all_colors)])
+        outfile.write(',h_std,s_std,l_std,s_avg,l_avg,s_min,l_min,s_max,l_max\n')
 
         for e in encoded_colors:
             outfile.write(f"{e['id']},")
             outfile.write('1,' if e['selected'] else '0,')
             outfile.write(','.join(str(e['colors'][c]) for c in all_colors))
+            outfile.write(f",{e['h_std']},{e['s_std']},{e['l_std']}")
+            outfile.write(f",,{e['s_avg']},{e['l_avg']},{e['s_min']},{e['l_min']},{e['s_max']},{e['l_max']}")
+
             outfile.write('\n')
 
 
---
+++ train/002-stats.py
@@ -0,0 +1,101 @@
+# initial train
+# score 0.410
+# minimize score
+
+import json
+import sys  # noqa
+import warnings
+from time import time
+from pprint import pprint  # noqa
+import lightgbm as lgb
+import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+
+pd.options.display.float_format = '{:.4f}'.format
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 2000)
+np.set_printoptions(threshold=sys.maxsize)
+
+warnings.filterwarnings(module='sklearn*', action='ignore', category=DeprecationWarning)
+
+zipext = ''
+train_file = 'manually-selected-palettes'
+test_file = 'test-palettes'
+
+start_time = time()
+last_time = time()
+
+
+def timer():
+    global last_time
+
+    print(f'{((time() - last_time) / 60):.1f} mins\n')  # noqa
+
+    last_time = time()
+
+
+def evaluate(train, test, unique_id, target):
+
+    print('evaluate')
+
+    lgb_model = lgb.LGBMClassifier(nthread=4, n_jobs=-1, verbose=-1, metric='rmse', objective='binary')
+
+    x_train = train.drop([target, unique_id], axis=1)
+    y_train = train[target]
+
+    x_test = test[x_train.columns]
+
+    lgb_model.fit(x_train, y_train)
+
+    train_predictions = lgb_model.predict(x_train)
+    test_predictions = lgb_model.predict(x_test)
+
+    train_score = np.sqrt(mean_squared_error(train_predictions, y_train))
+
+    timer()
+
+    return test_predictions, train_score
+
+
+# --------------------- run
+
+
+def run():
+
+    unique_id = 'id'
+    target = 'selected'
+
+    # load data
+
+    train = pd.read_csv(f'../data/{train_file}.csv{zipext}')
+    test = pd.read_csv(f'../data/{test_file}.csv{zipext}')
+    test_json = json.load(open(f'../data/{test_file}.json'))
+
+    # ----------
+
+    test_predictions, train_score = evaluate(train, test, unique_id, target)
+
+    print('score', train_score)
+
+    test[target] = test_predictions
+
+    # save predictions to csv
+    test.to_csv(f'../data/predictions.csv', index=False)
+
+    # save predictions back to json
+    # assumes same sequence
+
+    for i in range(0, len(test_json)):
+        test_json[i]['selected'] = False if test.iloc[i].selected == 0 else True
+
+    with open(f'../data/predictions.json', 'w') as outfile:
+        json.dump(test_json, outfile, indent=4)
+
+
+# -------- main
+
+
+run()
+
+print(f'Finished {((time() - start_time) / 60):.1f} mins\a')
