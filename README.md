8efcccc67f97d7970f630368709a12058af469a3
Peter Orum <petorum0@publicisgroupe.net>
2020-02-05 13:58:30 +1100

    remove facebook post

--- README.md
+++ README.md
@@ -3,8 +3,11 @@
 Various bits of code to train on colour palettes, analyse fractals generated from https://github.com/peterorum/functal, use Microsoft Vision API tp genertae captions, and then post to socials from lambda.
 
 ### notes on processing functals
-* From the `data/hsl-json` folder, `scp functal://data/hsl-json/* .`
-* From the `generate` folder, run `combine-jsons.py`, 
-* then `convert-to-csv`.
-* From the `train` folder, run `003-split`.
-* From the `classify` folder, run `node server.js` using the `predictions` file as test.
+
+- From the `data/hsl-json` folder, `scp functal://data/hsl-json/* .`
+- From the `generate` folder, run `combine-jsons.py`,
+- then `convert-to-csv`.
+- From the `train` folder, run `003-split`.
+- From the `classify` folder, run `node server.js` using the `predictions` file as test.
+
+For lambda, put the files in captions along with node_modules in the same folder & compress, then upload zip.
--- captions/daily-functal.js
+++ captions/daily-functal.js
@@ -1,41 +1,41 @@
-const graph = require('fbgraph')
-const tumblr = require('tumblr.js')
+const graph = require("fbgraph");
+const tumblr = require("tumblr.js");
 
-const s3 = require('./s3-client')
-const getCaption = require('./get-caption').getCaption
-const twit = require('./tweet-media')
+const s3 = require("./s3-client");
+const getCaption = require("./get-caption").getCaption;
+const twit = require("./tweet-media");
 
 // post to facebook
 
 function posttofacebook(imageUrl, message) {
   return new Promise(function(resolve, reject) {
-    graph.setAccessToken(process.env.fb_df_access_token)
+    graph.setAccessToken(process.env.fb_df_access_token);
 
     // get page accounts
-    graph.get('me/accounts', function(err, res) {
-      const fbpage = res.data.find(p => p.name === 'Daily Functal')
+    graph.get("me/accounts", function(err, res) {
+      const fbpage = res.data.find(p => p.name === "Daily Functal");
 
       // change access token to page's
-      graph.setAccessToken(fbpage.access_token)
+      graph.setAccessToken(fbpage.access_token);
 
       var post = {
         message,
         url: imageUrl
-      }
+      };
 
       // post to page photos
-      graph.post('/' + fbpage.id + '/photos', post, function(err, res) {
-        console.log('facebook', res)
+      graph.post("/" + fbpage.id + "/photos", post, function(err, res) {
+        console.log("facebook", res);
 
         if (err) {
-          console.log(err)
-          reject(err)
+          console.log(err);
+          reject(err);
         } else {
-          resolve()
+          resolve();
         }
-      })
-    })
-  })
+      });
+    });
+  });
 }
 
 // tumblr
@@ -47,81 +47,82 @@ function posttotumblr(message, imageUrl) {
       consumer_secret: process.env.tumblr_df_consumer_secret,
       token: process.env.tumblr_df_token,
       token_secret: process.env.tumblr_df_token_secret
-    })
+    });
 
     var options = {
       caption: message,
       source: imageUrl
-    }
+    };
 
-    tumblrClient.createPhotoPost('functal', options, function(err, data) {
+    tumblrClient.createPhotoPost("functal", options, function(err, data) {
       if (err) {
-        console.log(err)
-        reject(err)
+        console.log(err);
+        reject(err);
       }
 
-      console.log('tumblr', data)
-      resolve()
-    })
-  })
+      console.log("tumblr", data);
+      resolve();
+    });
+  });
 }
 
 // get random image & caption
 
 function dailyFunctal() {
   return new Promise(function(resolve, reject) {
-    s3.list('functals').then(function(result) {
+    s3.list("functals").then(function(result) {
       if (result.count === 0) {
-        console.log('No files')
+        console.log("No files");
       } else {
-        const filenames = result.files.map(f => f.Key)
+        const filenames = result.files.map(f => f.Key);
 
-        const filename = filenames[Math.floor(Math.random() * filenames.length)]
+        const filename =
+          filenames[Math.floor(Math.random() * filenames.length)];
 
-        const bucket = 'functals'
+        const bucket = "functals";
 
-        const imageUrl = `https://${bucket}.s3.amazonaws.com/${filename}`
+        const imageUrl = `https://${bucket}.s3.amazonaws.com/${filename}`;
 
         getCaption(imageUrl).then(
           caption => {
-            console.log(`${imageUrl} ${caption}`)
+            console.log(`${imageUrl} ${caption}`);
 
             if (caption) {
-              const message = `"${caption}" #fractal #functal #digitalart`
+              const message = `"${caption}" #fractal #functal #digitalart`;
 
-              posttofacebook(imageUrl, message).then(
+              // posttofacebook(imageUrl, message).then(
+              //   () => {
+              posttotumblr(message, imageUrl).then(
                 () => {
-                  posttotumblr(message, imageUrl).then(
-                    () => {
-                      // download file to /tmp
+                  // download file to /tmp
 
-                      const tmpFile = '/tmp/functal.jpg'
+                  const tmpFile = "/tmp/functal.jpg";
 
-                      s3.download(bucket, filename, tmpFile).then(function() {
-                        twit.tweet(message, tmpFile).then(
-                          () => {
-                            resolve()
-                          },
-                          () => {
-                            reject()
-                          }
-                        )
-                      })
-                    },
-                    () => {}
-                  )
+                  s3.download(bucket, filename, tmpFile).then(function() {
+                    twit.tweet(message, tmpFile).then(
+                      () => {
+                        resolve();
+                      },
+                      () => {
+                        reject();
+                      }
+                    );
+                  });
                 },
                 () => {}
-              )
+              );
+              //   },
+              //   () => {}
+              // )
             } else {
-              reject('no caption')
+              reject("no caption");
             }
           },
           err => console.error(err)
-        )
+        );
       }
-    })
-  })
+    });
+  });
 }
 
-exports.dailyFunctal = dailyFunctal
+exports.dailyFunctal = dailyFunctal;
